f901d7b88fdf49f71c9fcd46018e64767b1aefba
DrakeLopez27 <121637335+DrakeLopez27@users.noreply.github.com>
2023-01-05 11:09:43 -0600

    wrote console line

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -3,8 +3,15 @@
     public class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+           
+        {string dogName = "Ralph";
+            int dogAge = 10;
+            char[] value = { 'a' };
+            bool ifDog = true;
+            double dogWeight = 10.25;
+            decimal dogHeight = 3.35m;
+
+            Console.WriteLine($"My dog's name is {dogName}, He is {dogAge} years old");
         }
     }
 }
